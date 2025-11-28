7ac6c6a58d46a89b103b9e46d8d79c127e0fea20
makej7403-bit <makej7403@gmail.com>
2025-11-28 08:24:03 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'ma-blue': '#0b6cff',
+        'ma-teal': '#00bfa6',
+        'ma-dark': '#0f1724'
+      }
+    }
+  },
+  plugins: []
+}
